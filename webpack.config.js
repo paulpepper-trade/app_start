62b14ea724f4a33f3d3ea47c1c5fdb945b362530
Paul Pepper <paul.pepper@digital.trade.gov.uk>
2022-10-12 16:16:57 +0100

    Quieten sass compiler deprecation warnings about division.

--- webpack.config.js
+++ webpack.config.js
@@ -62,6 +62,7 @@ module.exports = {
               sassOptions: {
                 includePaths: [
                 ],
+                quietDeps: true
               },
             },
           },
